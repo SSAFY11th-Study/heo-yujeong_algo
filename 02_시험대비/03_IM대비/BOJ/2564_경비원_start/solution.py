8c71f600fa6c6c03f4a94a2abe0d9c2961d7a394
heo-yujeong <yu.jeong5899@gmail.com>
2024-02-29 01:05:06 +0900

    im 문제 풀이 ing_4

--- 02_시험대비/03_IM대비/BOJ/2564_경비원_start/solution.py
+++ 02_시험대비/03_IM대비/BOJ/2564_경비원_start/solution.py
@@ -1,2 +1,8 @@
 import sys
 sys.stdin = open('input.txt')
+
+garo, sero = map(int, input().split())
+N = int(input())
+store = [list(map(int, input().split())) for _ in range(N)]
+spot = list(map(int, input().split()))
+
